44bb8830c55cc6fc89ae4a83e270644d15e51fb0
Harsh vardhan <158584751+HarshVardhan456@users.noreply.github.com>
2025-03-25 18:52:13 +0530

    Add files via upload

---
+++ gitprofile.config.ts
@@ -0,0 +1,178 @@
+// gitprofile.config.ts
+
+const CONFIG = {
+  github: {
+    username: 'harshVardhan456', // Your GitHub org/user name. (This is the only required config)
+  },
+  /**
+   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
+   * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
+   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+   */
+  base: '/gitprofile/',
+  projects: {
+    github: {
+      display: true, // Display GitHub projects?
+      header: 'Github Projects',
+      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+      automatic: {
+        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+        limit: 8, // How many projects to display.
+        exclude: {
+          forks: false, // Forked projects will not be displayed if set to true.
+          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        },
+      },
+      manual: {
+        // Properties for manually specifying projects
+        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+      },
+    },
+    external: {
+      header: 'My Projects',
+      // To hide the `External Projects` section, keep it empty.
+      projects: [
+        {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        },
+        {
+          title: 'Project Name',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+          imageUrl:
+            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
+          link: 'https://example.com',
+        },
+      ],
+    },
+  },
+  seo: {
+    title: 'Portfolio of Harsh vardhan',
+    description: '',
+    imageURL: '',
+  },
+  social: {
+    linkedin: 'harsh-vardhan-3bbb0624a',
+    researchGate: '',
+    facebook: '',
+    instagram: '',
+    reddit: '',
+    threads: '',
+    youtube: '', // example: 'pewdiepie'
+    udemy: '',
+    dribbble: '',
+    behance: '',
+    stackoverflow: '', // example: '1/jeff-atwood'
+    skype: '',
+    telegram: '',
+    phone: '9548681962',
+    email: 'harshsinghaniap00@gmail.com',
+  },
+  resume: {
+    fileUrl:
+      'https://drive.google.com/file/d/102051AxBdMB9T6PVmIuE9DiETj3UoYS7/view', // Empty fileUrl will hide the `Download Resume` button.
+  },
+  skills: [
+   "c++","python","Javascript","Typescript","Node js","tailwind css","SQL", "database","HTML","React","Flask","Data structure","Kubrenetes","docker","Grafana","Prometheus","jenkins", "cassandra"
+  ],
+  experiences: [
+    {
+      company: 'Bosscoder Academy',
+      position: 'SDE',
+      from: 'November-2024',
+      to: 'Present',
+    },
+    {
+      company: 'noQs digital',
+      position: 'SDE intern',
+      from: 'june-2024',
+      to: 'August-2024',
+    },
+  ],
+  educations: [
+    {
+      institution: 'NIT Delhi',
+      degree: 'Bachelor of Technology',
+      from: '2021',
+      to: '2025',
+    },
+  ],
+  themeConfig: {
+    defaultTheme: 'lofi',
+
+    // Hides the switch in the navbar
+    // Useful if you want to support a single color mode
+    disableSwitch: false,
+
+    // Should use the prefers-color-scheme media-query,
+    // using user system preferences, instead of the hardcoded defaultTheme
+    respectPrefersColorScheme: false,
+
+    // Display the ring in Profile picture
+    displayAvatarRing: true,
+
+    // Available themes. To remove any theme, exclude from here.
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
+      'procyon',
+    ],
+
+    // Custom theme, applied to `procyon` theme
+    customTheme: {
+      primary: '#fc055b',
+      secondary: '#219aaf',
+      accent: '#e8d03a',
+      neutral: '#2A2730',
+      'base-100': '#E3E3ED',
+      '--rounded-box': '3rem',
+      '--rounded-btn': '3rem',
+    },
+  },
+
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Made with <a 
+      class="text-primary" href="https://github.com/HarshVardhan456"
+      target="_blank"
+      rel="noreferrer"  
+    >GitProfile</a> and $`,
+
+  enablePWA: true,
+};
+
+export default CONFIG;
